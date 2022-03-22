2f99c47556206c7a87b1301090ee7aac7a66bca9
Puneet Sandhu <puneet_sandhu@sfu.ca>
2022-03-22 15:15:33 -0700

    added landing page animation

--- public/anime.js
+++ public/anime.js
@@ -4,3 +4,24 @@ anime({
     direction: 'alternate',
     easing: 'easeInOutSine'
   });
+
+
+const button = document.querySelector('button');
+
+
+const timeline = anime.timeline({
+    easing: 'linear',
+});
+
+
+
+// show the dashed line
+timeline.add({
+    targets: '.in-between',
+    strokeDashoffset: [0, anime.setDashoffset],
+    duration: 2000,
+    delay: 2000
+})
+
+
+
