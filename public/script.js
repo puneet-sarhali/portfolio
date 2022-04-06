d126bba0fbaec9a11ca6e3b4d3e33ba9ed2d499d
Puneet Sandhu <puneet_sandhu@sfu.ca>
2022-04-06 00:10:45 -0700

    responsiveness fixes and images added

--- public/script.js
+++ public/script.js
@@ -1,6 +1,10 @@
 var detailBtn = document.querySelector('.detail-btn');
+
+var detail = document.querySelector(".details");
+var displaySetting = detail.style.display;
+detail.style.display = 'none';
+
 detailBtn.addEventListener('click',()=> {
-    console.log('working')
     var detail = document.querySelector(".details");
     var displaySetting = detail.style.display;
     if (displaySetting == 'block') {
